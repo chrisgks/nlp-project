3b5a284bcd16fb16c2b3d292276c9ce470e8c165
chr1ss <chris.gkika9619@gmail.com>
2020-07-02 17:26:17 +0100

    added vectorize() to vectorize strings using selected word embedding models

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,7 +1,63 @@
-from flair.data import Sentence
 from flair.models import SequenceTagger
+from flair.embeddings import WordEmbeddings
+from flair.embeddings import CharacterEmbeddings
+from flair.embeddings import FlairEmbeddings
+from flair.embeddings import TransformerWordEmbeddings
+from flair.embeddings import DocumentPoolEmbeddings
+from flair.data import Sentence
 
-# load the NER tagger
+# constructing all the models onnce and not loading a new one at every function call; saves a lot of ram; faster
+bert_embedding = TransformerWordEmbeddings('bert-base-cased')
+roberta_embedding = TransformerWordEmbeddings('roberta-base')
+glove_embedding = WordEmbeddings('glove')
+character_embeddings = CharacterEmbeddings()
+flair_forward = FlairEmbeddings('news-forward-fast')
+flair_backward = FlairEmbeddings('news-backward-fast')
+
+
+def vectorize(string: str, args: list = None):
+    """
+
+    :param string:
+    :param args:
+    :return:
+    """
+
+    if not args:
+        raise SystemExit(f"[ERROR]: function {vectorize.__name__}() -> Provide at least one base model: ['bert',"
+                         f"'roberta', 'glove', 'character', 'flair_forward', 'flair_backward']")
+
+    embeddings = []
+
+    if 'bert' in args:
+        embeddings.append(bert_embedding)
+    if 'roberta' in args:
+        embeddings.append(roberta_embedding)
+    if 'glove' in args:
+        embeddings.append(glove_embedding)
+    if 'character' in args:
+        embeddings.append(character_embeddings)
+    if 'flair_forward' in args:
+        embeddings.append(flair_forward)
+    if 'flair_backward' in args:
+        embeddings.append(flair_backward)
+
+    # if none of the above, then the model combination passed is not supported
+    if not embeddings:
+        raise SystemExit(f"[ERROR]: function {vectorize.__name__}() -> {args} not available. Supported models: "
+                         f"['bert', 'roberta', 'glove', 'character', 'flair_forward', 'flair_backward']")
+
+    stacked_embeddings = DocumentPoolEmbeddings(embeddings=embeddings,
+                                                fine_tune_mode='none',
+                                                pooling='mean')
+
+    sentence = Sentence(string)
+    stacked_embeddings.embed(sentence)
+
+    return sentence.embedding.detach().numpy()
+
+
+# loading the NER tagger once and not loading a new one at every function call; saves some ram; faster
 tagger = SequenceTagger.load('ner')
 
 
@@ -33,3 +89,5 @@ def has_digit(string: str):
     return any(char.isdigit() for char in string)
 
 
+def save_output_to_json():
+    pass
